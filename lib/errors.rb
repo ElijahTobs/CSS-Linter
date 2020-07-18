2c968545b6b6b0910cd21fdabfcd694fb9ffda19
Elijah <eligenius@gmail.com>
2020-07-18 16:27:39 +0100

    fix rubocop errors

--- lib/errors.rb
+++ lib/errors.rb
@@ -4,20 +4,20 @@ module ErrorsModule
   def space_before_semicolon?(char, char_index)
     return true unless char.match?(/( ;)+/)
 
-    puts 'Error:'.red + 'on' + "line:[:#{char_index}:]".yellow. + ' Unexpected space before semicolon ";" '
+    puts 'Error'.red + ' on' + " line:[:#{char_index}:]".yellow + ' Unexpected space before semicolon ";"'
   end
 
-  def indentation?(char, char_index)
-    return true if str.match?(/^(\s{2})+([a-zA-Z0-9\-_]+)/)
+  def indentation?(_char, char_index)
+    return true if char.match?(/^(\s{2})+([a-zA-Z0-9\-_]+)/)
 
-    puts 'Error'.red + ' on' + " line:[:#{char_indx}:]".yellow + ' Expecting indentation to be two spaced'
+    puts 'Error'.red + ' on' + " line:[:#{char_index}:]".yellow + ' Expecting indentation to be two spaced'
   end
 
   def trailing_white_space?(char, char_index)
     if !char.match?(/(?<=\S)[\t ]+$/) || char.match?(/^\n$/)
       true
     else
-      puts 'Error'.red + ' on' + " line:[:#{data_index}:]".yellow + ' Unexpected white space'
+      puts 'Error'.red + ' on' + " line:[:#{char_index}:]".yellow + ' Unexpected white space'
     end
   end
 
@@ -32,12 +32,12 @@ module ErrorsModule
   def space_before_colon?(char, char_index)
     return true unless char.match?(/( :)/)
 
-    puts 'Error'.red + ' on' + " line:[:#{char_index}:]".yellow + 'Unexpexted space before colon ":" '
+    puts 'Error'.red + ' on' + " line:[:#{char_index}:]".yellow + ' Unexpexted space before colon ":"'
   end
-  
+
   def space_after_colon?(char, char_index)
     return true unless char.match?(/(:\w+)/)
-    
+
     puts 'Error'.red + ' on' + " line:[:#{char_index}:]".yellow + 'Unexpexted space after colon ":" '
   end
 end
