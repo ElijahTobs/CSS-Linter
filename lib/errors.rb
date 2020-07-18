7a4fd2610e3a066efbe738f93d20b989396e50b9
Elijah <eligenius@gmail.com>
2020-07-18 18:29:02 +0100

    Refactor errors.rb

--- lib/errors.rb
+++ lib/errors.rb
@@ -1,43 +1,43 @@
 require 'colorize'
 
 module ErrorsModule
-  def space_before_semicolon?(char, char_index)
-    return true unless char.match?(/( ;)+/)
+  def space_before_semicolon?(chars, chars_index)
+    return true unless chars.match?(/( ;)+/)
 
-    puts 'Error'.red + ' on' + " line:[:#{char_index}:]".yellow + ' Unexpected space before semicolon ";"'
+    puts 'Error'.red + ' on' + " line:[:#{chars_index}:]".yellow + ' Unexpected space before semicolon ";"'
   end
 
-  def indentation?(_char, char_index)
-    return true if char.match?(/^(\s{2})+([a-zA-Z0-9\-_]+)/)
+  def indentation?(_chars, chars_index)
+    return true if chars.match?(/^(\s{2})+([a-zA-Z0-9\-_]+)/)
 
-    puts 'Error'.red + ' on' + " line:[:#{char_index}:]".yellow + ' Expecting indentation to be two spaced'
+    puts 'Error'.red + ' on' + " line:[:#{chars_index}:]".yellow + ' Expecting indentation to be two spaced'
   end
 
-  def trailing_white_space?(char, char_index)
-    if !char.match?(/(?<=\S)[\t ]+$/) || char.match?(/^\n$/)
+  def trailing_white_space?(chars, chars_index)
+    if !chars.match?(/(?<=\S)[\t ]+$/) || chars.match?(/^\n$/)
       true
     else
-      puts 'Error'.red + ' on' + " line:[:#{char_index}:]".yellow + ' Unexpected white space'
+      puts 'Error'.red + ' on' + " line:[:#{chars_index}:]".yellow + ' Unexpected white space'
     end
   end
 
-  def ending_semicolon?(char, char_index)
-    if char.match?(/(;.*)$/) || char.match?(/({|})/) || char.match?(/^\n$/)
+  def ending_semicolon?(chars, chars_index)
+    if chars.match?(/(;.*)$/) || chars.match?(/({|})/) || chars.match?(/^\n$/)
       true
     else
-      puts 'Error'.red + ' on' + " line:[:#{char_index}:]".yellow + ' Missing ending semicolon ";" '
+      puts 'Error'.red + ' on' + " line:[:#{chars_index}:]".yellow + ' Missing ending semicolon ";" '
     end
   end
 
-  def space_before_colon?(char, char_index)
-    return true unless char.match?(/( :)/)
+  def space_before_colon?(chars, chars_index)
+    return true unless chars.match?(/( :)/)
 
-    puts 'Error'.red + ' on' + " line:[:#{char_index}:]".yellow + ' Unexpexted space before colon ":"'
+    puts 'Error'.red + ' on' + " line:[:#{chars_index}:]".yellow + ' Unexpexted space before colon ":"'
   end
 
-  def space_after_colon?(char, char_index)
-    return true unless char.match?(/(:\w+)/)
+  def space_after_colon?(chars, chars_index)
+    return true unless chars.match?(/(:\w+)/)
 
-    puts 'Error'.red + ' on' + " line:[:#{char_index}:]".yellow + 'Unexpexted space after colon ":" '
+    puts 'Error'.red + ' on' + " line:[:#{chars_index}:]".yellow + 'Unexpexted space after colon ":" '
   end
 end
--- lib/errors_checker.rb
+++ lib/errors_checker.rb
@@ -2,13 +2,32 @@ require_relative 'errors.rb'
 
 module ErrorsChecker
   include ErrorsModule
-  attr_reader :error_array
+  attr_reader :chars_array
 
-  def initialize(error_array)
-    @error_array = error_array
+  def initialize(chars_array)
+    @chars_array = chars_array
   end
 
-  
+  def inspect
+    chars_array.each_with_index do |chars, index|
+      
+    end
+    
+  end
+
 
 
 end
+
+# arr = [2,4,3,1,7,5,9]
+# require 'colorize'
+
+# def test(num)
+#   unless num.even?
+#     "Number is odd".cyan
+#   else
+#     "Number is even".green
+#   end
+# end
+
+# puts test(6)
