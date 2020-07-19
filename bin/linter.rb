65abdb5c3dc8982f7b5bbc5112300dc249e8e815
Elijah <eligenius@gmail.com>
2020-07-19 13:40:49 +0100

    Add logic to read and inspect userfile in linter.rb

--- bin/linter.rb
+++ bin/linter.rb
@@ -12,7 +12,11 @@ puts
 puts 'Scanning to detect possible errors...'.cyan
 sleep(3)
 
-
+file_path = ARGV
+user_file = File.open(file_path)
+file_data = user_file.readlines
+checks = ErrorsChecker.new(file_data)
+checks.inspect
 
 
 puts 'Scan Complete!'.green.hide
