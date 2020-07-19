c30d4c12185ba9d1f01c18016cfcc4f3b17598c2
Elijah <eligenius@gmail.com>
2020-07-19 12:59:24 +0100

    Add rainbow gem

---
+++ bin/linter.rb
@@ -0,0 +1,20 @@
+require 'rainbow/refinement'
+using Rainbow
+# require_relative 'errors_checker.rb'
+
+puts '----------------------------'.blue
+puts
+puts '|| Welcome to CSS Linter! ||'.green.bright.underline
+puts
+puts '----------------------------'.blue
+sleep(1)
+puts
+puts 'Scanning to detect possible errors...'
+puts
+sleep(3)
+
+file_path = ARGV
+
+
+
+puts 'Scanning Complete!'.green.hide
