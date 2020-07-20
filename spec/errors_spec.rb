737d9c9cdef19fb5d8105c838a382327de8dbf82
Elijah <eligenius@gmail.com>
2020-07-20 13:56:31 +0100

    Add test for #space_before_semicolon

--- spec/errors_spec.rb
+++ spec/errors_spec.rb
@@ -6,11 +6,21 @@ RSpec.describe ErrorsModule do
   let(:correct_format) { '  font-size: 16px;' } 
   
   
+  describe "#space_before_semicolon?" do
+    let(:wrong_format) { 'font-size: 16px ;' } 
+    it "returns true if there's no space before semicolon" do
+      expect(space_before_semicolon?(correct_format, chars_index)).to eql(true)
+    end
+
+    it "returns error message if space found before semicolon" do
+      expect {space_before_semicolon?(wrong_format, chars_index)}.to output(String).to_stdout
+    end
+  end
+  
   
   describe '#indentation?' do
     let(:wrong_format) { 'font-size: 16px;' } 
     it 'returns true if the declared property has two spaced indentation' do
-      f_string = 'font-size: 14px;'
       expect(indentation?(correct_format, chars_index)).to eql(true)
     end
 
