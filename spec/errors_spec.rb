3e143912d35127851b31aa84924862ce291bb18a
Elijah <eligenius@gmail.com>
2020-07-20 14:51:08 +0100

    Add test for #ending_semicolon

--- spec/errors_spec.rb
+++ spec/errors_spec.rb
@@ -17,7 +17,6 @@ RSpec.describe ErrorsModule do
     end
   end
   
-  
   describe '#indentation?' do
     let(:wrong_format) { 'font-size: 16px;' } 
     it 'returns true if the declared property has two spaced indentation' do
@@ -27,17 +26,31 @@ RSpec.describe ErrorsModule do
     it 'prints error message if declared property is without two spaced indentation' do
       expect {indentation?(wrong_format, chars_index)}.to output(String).to_stdout
     end
+  end
 
-    describe "#trailing_white_space?" do
-      let(:wrong_format) { 'font-size: 16px;  ' }
-      it "returns true if no white space found" do
-        expect(trailing_white_space?(correct_format, chars_index)).to eql(true)
-      end
-      
-      it "returns error message if white space found" do
-        expect {trailing_white_space?(wrong_format, chars_index)}.to output(String).to_stdout
-      end
+  describe "#trailing_white_space?" do
+    let(:wrong_format) { 'font-size: 16px;  ' }
+    it "returns true if no white space found" do
+      expect(trailing_white_space?(correct_format, chars_index)).to eql(true)
     end
     
+    it "returns error message if white space found" do
+      expect {trailing_white_space?(wrong_format, chars_index)}.to output(String).to_stdout
+    end
   end
+
+  describe "#ending_semicolon?" do
+    let(:wrong_format) { 'font-size: 16px' } 
+    it "returns true if ending semicolon is present" do
+      expect(ending_semicolon?(correct_format, chars_index)).to eql(true)
+    end
+
+    it "returns error message if ending semicolon is not present" do
+      expect {ending_semicolon?(wrong_format, chars_index)}.to output(String).to_stdout
+    end
+    
+    
+  end
+  
+
 end
