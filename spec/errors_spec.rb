01052ede6fab18c28fbf974c10059995c4cc0165
Elijah <eligenius@gmail.com>
2020-07-20 14:24:45 +0100

    Add test for #trailing_white_space

--- spec/errors_spec.rb
+++ spec/errors_spec.rb
@@ -27,5 +27,17 @@ RSpec.describe ErrorsModule do
     it 'prints error message if declared property is without two spaced indentation' do
       expect {indentation?(wrong_format, chars_index)}.to output(String).to_stdout
     end
+
+    describe "#trailing_white_space?" do
+      let(:wrong_format) { 'font-size: 16px;  ' }
+      it "returns true if no white space found" do
+        expect(trailing_white_space?(correct_format, chars_index)).to eql(true)
+      end
+      
+      it "returns error message if white space found" do
+        expect {trailing_white_space?(wrong_format, chars_index)}.to output(String).to_stdout
+      end
+    end
+    
   end
 end
